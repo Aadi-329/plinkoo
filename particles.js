2216a573fd302ab3e61d734ea9077331854d3062
aadityasalecha <67157010+aadityasalecha@users.noreply.github.com>
2020-07-13 18:52:37 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -2,7 +2,7 @@ class particle{
 
 
     constructor(x,y){
-    this.particle=Bodies.circle(x,y,5,{Restitution:0.4})
+    this.particle=Bodies.circle(x,y,10,{Restitution:0.4})
     this.colour=color(random(0,255),random(0,255),random(0,255))
     World.add(world,this.particle)
     
@@ -18,7 +18,7 @@ class particle{
     ellipseMode(RADIUS)
     
     fill(this.colour);
-    ellipse(0,0,5,5)
+    ellipse(0,0,10,10)
     pop();
     
     }
